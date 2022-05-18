2723f93a1a10cbb3a9a2727052cc34d1ddc6cae3
Fuad <fuadadio75@gmail.com>
2022-05-18 23:33:16 +0100

    setting up the homepage

--- savecloud/__init__.py
+++ savecloud/__init__.py
@@ -6,8 +6,8 @@ from .config import Config
 
 
 db= MongoEngine()
-login_manager= LoginManager()
-login_manager.login_view= 'users.login'
+# login_manager= LoginManager()
+# login_manager.login_view= 'users.login'
 
 
 
@@ -19,7 +19,7 @@ def create_app(config_class= Config):
     app.config.from_object(Config)
 
     db.init_app(app)
-    login_manager.init_app(app)
+    # login_manager.init_app(app)
 
     from savecloud.main.routes import main
 
--- savecloud/config.py
+++ savecloud/config.py
@@ -1,4 +1,5 @@
 import os
 class Config:
     MONGODB_HOST = os.environ.get('SAVECLOUD_DATABASE_URI')
+    print(MONGODB_HOST)
     SECRET_KEY= os.environ.get('FLASK_SECRET_KEY')
