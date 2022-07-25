3529ec6649aa2b919d32e09d655f00ffd1ecd4b1
Fuad <fuadadio75@gmail.com>
2022-07-25 12:25:49 +0100

    implemented custom unauthorized_loader error message

--- cloudbox/store/utils.py
+++ cloudbox/store/utils.py
@@ -1,5 +1,7 @@
 from flask_restful import marshal
+from flask import jsonify
 
+from cloudbox import jwt_manager
 from .fields import folder_asset_fields, file_asset_fields
 
 def combined_folder_file_response(qs):
@@ -17,3 +19,9 @@ def single_entity_response(entity):
         return marshal(entity, folder_asset_fields) | {"uri": entity.get_uri()}
     else:
         return marshal(entity, file_asset_fields) | {"uri": entity.get_uri()}
+
+
+# customise errror message
+@jwt_manager.unauthorized_loader
+def unauthorized_loader_callback(arg_1):
+    return jsonify({"msg": "Missing credentials"})
